caa4bfd0c58ddcfd3046e5c73e5aefac7e12fb17
FelipeBrenner <felipeobrenner@hotmail.com>
2022-06-09 16:08:43 -0300

    feat: criando estrutura header e post

---
+++ ignite-feed/src/components/Comment/index.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/felipebrenner.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Felipe Brenner</strong>
+              <time title="11 de Maio às 17:23h" dateTime="2022-05-11 17:23:03">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
