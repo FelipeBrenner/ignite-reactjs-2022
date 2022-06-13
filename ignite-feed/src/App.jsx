4b477c2f6869707b70083ab9c4be42050eaa976d
FelipeBrenner <felipeobrenner@hotmail.com>
2022-06-13 11:06:24 -0300

    Propriedades do Post

--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -6,6 +6,43 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/felipebrenner.png",
+      name: "Felipe Brenner",
+      role: "React Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto pro meu porfifólio.",
+      },
+      { type: "link", content: "#nttdata" },
+    ],
+    publishedAt: new Date("2022-06-10 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/gabriellmilani.png",
+      name: "Gabriel Milani",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto pro meu porfifólio.",
+      },
+      { type: "link", content: "#cwi" },
+    ],
+    publishedAt: new Date("2022-06-13 20:00:00"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -14,9 +51,13 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map((post) => (
+            <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
--- ignite-feed/src/components/Post/index.jsx
+++ ignite-feed/src/components/Post/index.jsx
@@ -1,32 +1,53 @@
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { Avatar } from "../Avatar";
 import { Comment } from "../Comment";
 import styles from "./styles.module.css";
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/felipebrenner.png" />
+          <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>Felipe Brenner</strong>
-            <span>React Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Mairo às 11h13" dateTime="2022-05-11">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Teste</p>
-        <p>de</p>
-        <p>Parágrafo</p>
-        <p>
-          <a href="">#novoprojeto</a> <a href="">#ignite</a>
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
@@ -40,8 +61,6 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
         <Comment />
       </div>
     </article>
